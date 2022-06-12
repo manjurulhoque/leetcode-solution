4a7521b2bed730c786b68e90624ba3765074605d
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2022-06-12 14:58:59 +0600

    Time: 1066 ms (5.01%), Space: 26 MB (71.55%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,9 +1,5 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
-                return True
-        
-        return False
+        if len(nums) == len(set(nums)):
+            return False
+        return True
