3b08d084099752dc115051dc4195d3fdc283b1c6
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2024-10-24 12:15:11 +0600

    [175] [Time Beats: 70.64%] - LeetPush

---
+++ 175-Combine-Two-Tables.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+select p.firstName as firstName, p.lastName as lastName, a.city as city, a.state as state from Person as p LEFT JOIN Address as a ON p.personId=a.personId;
