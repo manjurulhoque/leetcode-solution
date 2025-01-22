f0f832a8430c6f4c91db5fc1cb70db990955070b
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2025-01-22 18:44:50 +0600

    [584] [Time Beats: 61.87%] - LeetPush

--- 584-Find-Customer-Referee.sql
+++ 584-Find-Customer-Referee.sql
@@ -1,3 +1,7 @@
+-- SELECT name 
+-- FROM Customer 
+-- WHERE referee_id != 2 OR referee_id IS NULL;
+
 SELECT name 
-FROM Customer 
-WHERE referee_id != 2 OR referee_id IS NULL;
+FROM Customer
+WHERE COALESCE(referee_id, 0) != 2;
