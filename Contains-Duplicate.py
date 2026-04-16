423037896d713a7410a33d01efb581bd18c1c4ce
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2026-04-16 12:39:44 +0600

    [217] [Time Beats: 7.23%] [Memory Beats: 94.26%] - LeetPush

--- Contains-Duplicate.py
+++ Contains-Duplicate.py
@@ -1,8 +1,8 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
-                return True
-        return False
+1class Solution:
+2    def containsDuplicate(self, nums: List[int]) -> bool:
+3        nums.sort()
+4
+5        for i in range(len(nums) - 1):
+6            if nums[i] == nums[i + 1]:
+7                return True
+8        return False
